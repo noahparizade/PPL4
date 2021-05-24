0ef6918a76c47f99219347a8293e446eba5e3524
NoaP <noahp@post.bgu.ac.il>
2021-05-24 13:10:12 +0300

    2.3

--- part2/src/part2.ts
+++ part2/src/part2.ts
@@ -33,24 +33,58 @@ type PromisedStore<K, V> = {
 /* 2.2 */
 
 // ??? (you may want to add helper functions here)
-//
-// export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
-//     ???
-// }
+
+export async function checkValue<T,R>(store:PromisedStore<T,R>, param:T):Promise<R> {
+         return await store.get(param); 
+}
+
+
+ export function asycMemo<T, R>(f: (param: T) => R): (param: T) => Promise<R> {
+    let store = makePromisedStore<T,R>();
+     let ans:(param:T) =>Promise<R> = (param:T) => {
+         return checkValue(store, param).then((val:R)=>val, ()=>{
+             let result:R = f(param);
+             store.set(param,result);
+             return result;
+         })
+     }
+     return ans
+ }
 
 /* 2.3 */
 
-// export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: ???): ??? {
-//     ???
-// }
+ export function lazyFilter<T>(genFn: () => Generator<T>, filterFn: (a:T[])=>T[]): ()=>Generator<T> {
+     let gen = genFn();
+     function* lazygen():Generator<T> {
+     for (let v of gen){
+         if (filterFn([v]).length>0) yield v  
+     }
+    }
+    let func = ()=>lazygen();
+    return func;
 
-// export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: ???): ??? {
-//     ???
-// }
+ }
+
+ export function lazyMap<T, R>(genFn: () => Generator<T>, mapFn: (a:T[])=>R[]): ()=>Generator<R> {
+    let gen = genFn();
+    function* lazygen():Generator<R> {
+    for (let v of gen){
+        yield mapFn([v])[0] 
+    }
+   }
+   let func = ()=>lazygen();
+   return func;
+ }
 
 /* 2.4 */
 // you can use 'any' in this question
 
-// export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, ...(???)[]]): Promise<any> {
-//     ???
-// }
+ export async function asyncWaterfallWithRetry(fns: [() => Promise<any>, (x:any)=>Promise<any>[]]): Promise<any> {
+     fns[0]().then((x: any)=> recWaterfall(fns[1], 0, 0, x), ()=> {
+         let p = Promise.resolve(())
+         setTimeout(p.then,2000)
+ }
+
+ export async function recWaterfall(fns: (x:any)=>Promise<any>[], i: number, numOfFails: number, x:any): Promise<any> {
+     return i===0
+ }
